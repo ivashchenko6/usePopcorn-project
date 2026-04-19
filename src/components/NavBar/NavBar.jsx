0a9daf1371596e43f19b6646873f98979592c89e
Kyryl Ivashchenko <love2dance@Kyryls-Laptop.lan>
2026-04-19 10:48:05 -0700

    created

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const NavBar = () => {
+    const [query, setQuery] = useState("");
+    return (
+        <nav className="nav-bar">
+            <div className="logo">
+                <span role="img">🍿</span>
+                <h1>usePopcorn</h1>
+            </div>
+            <input
+                className="search"
+                type="text"
+                placeholder="Search movies..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <p className="num-results">
+                Found <strong>X</strong> results
+            </p>
+        </nav>
+    );
+};
+
+export default NavBar;
