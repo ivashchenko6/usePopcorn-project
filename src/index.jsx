eede0600374b7b1b6754e52be9f6a66e1df6f18a
Kyryl  Ivashchenko <kirill040605@gmail.com>
2026-05-20 21:32:14 -0700

    119 lesson

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,10 @@ import StarRating from "./components/StarRating/StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <StarRating maxRating={10} />
+        <StarRating
+            maxRating={5}
+            messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        />
+        <StarRating size={24} color="red" className="test" />
     </React.StrictMode>,
 );
